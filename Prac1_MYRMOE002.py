57bc4a9ddb61d6bd4056a3996a786e2448cc291e
Rashaad <rashaad.meyer@gmail.com>
2019-07-28 19:09:04 +0100

    Modified Prac1_MYRMOE002.py

--- Prac1_MYRMOE002.py
+++ Prac1_MYRMOE002.py
@@ -9,38 +9,73 @@ Date: 28/07/2019
 """
 
 # import Relevant Librares
-import RPi.GPIO as GPIO
-from time import sleep
+import RPi.GPIO as GPIO 	# Import Raspberry Pi GPIO library
+from time import sleep  	# Import the sleep function from the time module
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setwarnings(False)		# Ignore warning for now
+GPIO.setmode(GPIO.BOARD)	# Use physical pin numbering
+GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)	# Set pin 16 to be an input pin and set pull_up_down resistor to down
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)	# Set pin 18 to be an input pin and set pull_up_down resistor to down
 
-GPIO.setup(11, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(13, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(15, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(11, GPIO.OUT, initial = GPIO.LOW)	# Set pin 11 to be an output pin and set initial value to low (off)
+GPIO.setup(13, GPIO.OUT, initial = GPIO.LOW)	# Set pin 13 to be an output pin and set initial value to low (off)
+GPIO.setup(15, GPIO.OUT, initial = GPIO.LOW)	# Set pin 15 to be an output pin and set initial value to low (off)
 
+binary = ["000", "001", "010", "011", "100", "101", "110", "111"]	# Initialises array that holds all binary values from 0-7
+counter = 0 	# Initialises counter
 
 def button_decrease_callback(channel):
-	print("decreased")
 
-	GPIO.output(11, GPIO.HIGH)
-	sleep(2)
+	global counter	# Allows the use of counter in this function
 
-	GPIO.output(11, GPIO.LOW)
-	sleep(2)
+	if counter == 0:	# Increments counter and resets if it gets to 0
+		counter = 7
+	else:
+		counter-=1
+
+	if binary[counter][0] == '1':	# Check if first bit is 1 and if it is turns first LED on else turns LED off
+		GPIO.output(11, GPIO.HIGH)
+	else:
+		GPIO.output(11, GPIO.LOW)
+
+	if binary[counter][1] == '1':	# Check if second bit is 1 and if it is turns second LED on else turns LED off
+                GPIO.output(13, GPIO.HIGH)
+        else:
+                GPIO.output(13, GPIO.LOW)
+
+	if binary[counter][2] == '1':	# Check if third bit is 1 and if it is turns third LED on else turns LED off
+                GPIO.output(15, GPIO.HIGH)
+        else:
+                GPIO.output(15, GPIO.LOW)
+
+	sleep(2)	# Sleeps program so that nothing happens for 2 seconds
+
 
 def button_increase_callback(channel):
-	print("increased")
 
-	GPIO.output(13, GPIO.HIGH)
-	GPIO.output(15, GPIO.HIGH)
-	sleep(2)
+	global counter	# Allows the use of counter in this function
 
-	GPIO.output(13, GPIO.LOW)
-	GPIO.output(15, GPIO.LOW)
-	sleep(2)
+	if counter == 7:	# Increments counter and resets if it gets to 0
+                counter = 0
+        else:
+                counter+=1
+
+        if binary[counter][0] == '1':	# Check if first bit is 1 and if it is turns first LED on else turns LED off
+                GPIO.output(11, GPIO.HIGH)
+        else:
+                GPIO.output(11, GPIO.LOW)
+
+        if binary[counter][1] == '1':	# Check if second bit is 1 and if it is turns second LED on else turns LED off
+                GPIO.output(13, GPIO.HIGH)
+        else:
+                GPIO.output(13, GPIO.LOW)
+
+        if binary[counter][2] == '1':	# Check if third bit is 1 and if it is turns third LED on else turns LED off
+                GPIO.output(15, GPIO.HIGH)
+        else:
+                GPIO.output(15, GPIO.LOW)
+	sleep(2)	# Sleeps program so that nothing happens for 2 seconds
+
 
 
 GPIO.add_event_detect(16, GPIO.RISING, callback = button_decrease_callback)
